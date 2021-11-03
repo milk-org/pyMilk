75468a7eb5a95f57479ff85bc199b6e2452d5bfe
vdeo <vdeo@naoj.org>
2021-11-02 19:05:27 -1000

    ZMQ shmim senders and receivers, very basic for now.

---
+++ pyMilk/scripts/zmq_recv.py
@@ -0,0 +1,74 @@
+'''
+zmq_recv.py
+
+Usage:
+    zmq_recv.py <host:port> <shm_name> [options]
+
+Options:
+    <host:port>     IP adress : port to receive from.
+    <shm_name>    SHM to listen for - carried over from source server
+    -s=<name_OR>  name override - reassign the SHM with a different name
+'''
+'''bash
+IP=""
+for CH in 00 01 02 03 04 05 06 07 08 09 10 11; do
+    zmq_recv.py ${IP}:321${CH} dm00disp${CH}
+done
+'''
+
+import zmq
+import pickle
+import numpy as np
+from typing import Tuple
+from docopt import docopt
+from pyMilk.interfacing.shm import SHM
+
+
+def zmq_recv_loop(host_port: Tuple[str, int], topic: str, out_name: str):
+
+    # Get the ZMQ side ready
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.setsockopt(
+            zmq.SUBSCRIBE,
+            topic.encode('ascii'))  # Subscribe to everything on this port.
+
+    socket.connect(f"tcp://{host_port[0]}:{host_port[1]}")
+
+    # We will get the pyMilk side ready in the loop upon the first reception
+    pymilk_ready = False
+    out_shm = None
+
+    while True:
+        message = socket.recv()
+        sp_idx = message.index(b' ')
+
+        keywords, data = pickle.loads(message[sp_idx + 1:])
+
+        if pymilk_ready:
+            out_shm.set_data(data)
+        else:
+            # Needs a little kick on dtype parsing o_O
+            out_shm = SHM(out_name,
+                          data=data.astype(np.dtype(data.dtype.name)),
+                          nbkw=len(keywords) * 2)
+            pymilk_ready = True
+
+        out_shm.set_keywords(keywords)
+
+
+if __name__ == "__main__":
+    # Parse
+    from docopt import docopt
+    doc = docopt(__doc__)
+
+    hp = doc['<host:port>'].split(':')
+    host = hp[0]
+    port = int(hp[1])
+    topic = doc['<shm_name>']
+
+    out_name = doc['-s']
+    if out_name is None:
+        out_name = topic
+
+    zmq_recv_loop((host, port), topic, out_name)
--- setup.py
+++ setup.py
@@ -14,18 +14,21 @@ from distutils.version import LooseVersion
 
 
 class CMakeExtension(Extension):
+
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
+
     def run(self):
         try:
             out = subprocess.check_output(['cmake', '--version'])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                    "CMake must be installed to build the following extensions: "
+                    + ", ".join(e.name for e in self.extensions))
 
         if platform.system() == "Windows":
             raise RuntimeError("Windows is not supported")
@@ -34,15 +37,21 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        extdir = os.path.abspath(
+                os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                '-DPYTHON_EXECUTABLE=' + sys.executable
+        ]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += [
+                    '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                            cfg.upper(), extdir)
+            ]
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
@@ -51,28 +60,32 @@ class CMakeBuild(build_ext):
             build_args += ['--', '-j2']
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+                env.get('CXXFLAGS', ''), self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
 
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-        name = 'pyMilk',
-        version = '1.01',
-        description = 'Python bindings and utilities to use with MILK and CACAO',
-        long_description = long_description,
-        author = 'Vincent Deo',
-        author_email = 'vdeo@naoj.org',
-        url = "http://www.github.com/milk-org/pyMilk",
-        packages = ['pyMilk'],  # same as name
-        install_requires = ['docopt', 'pyqtgraph', 'pybind11', 'numpy'],
-        ext_modules = [CMakeExtension('ImageStreamIO')],
-        cmdclass = dict(build_ext = CMakeBuild),
-        scripts = ['pyMilk/visual/shmPlot.py', 'pyMilk/visual/shmImshow.py'])
-
+        name='pyMilk',
+        version='1.01',
+        description='Python bindings and utilities to use with MILK and CACAO',
+        long_description=long_description,
+        author='Vincent Deo',
+        author_email='vdeo@naoj.org',
+        url="http://www.github.com/milk-org/pyMilk",
+        packages=['pyMilk'],  # same as name
+        install_requires=['docopt', 'pyqtgraph', 'pybind11', 'numpy'],
+        ext_modules=[CMakeExtension('ImageStreamIO')],
+        cmdclass=dict(build_ext=CMakeBuild),
+        scripts=[
+                'pyMilk/visual/shmPlot.py', 'pyMilk/visual/shmImshow.py',
+                'pyMilk/scripts/zmq_send.py', 'pyMilk/scripts/zmq_recv.py'
+        ])
