4100dd83ea782d4dcc4e1cc9d9c66a0e614ec11b
vdeo@scexao5 <vdeo@naoj.org>
2022-12-14 23:41:22 +0000

    Throttling in zmq_send

--- pyMilk/scripts/zmq_send.py
+++ pyMilk/scripts/zmq_send.py
@@ -8,12 +8,8 @@ Options:
     <host:port>     IP address : port to bind to.
     <shm_name>      SHMs to send
     -t <timeout>    Timeout to re-send data [default: -1]
+    -f <frate>      Throttling down to <frate> FPS
     -s <skip>       Send every n-th frame [default: 1]
-'''
-''' TBC
-    <shm_name>      One or more SHMs to send
-    <trig_name>     Trig SHM name - mandatory if more than one SHM name
-                    Must be a repeat of one of the SHM names
 '''
 '''bash
 IP=""
@@ -22,6 +18,7 @@ for CH in 00 01 02 03 04 05 06 07 08 09 10 11; do
 done
 '''
 
+import time
 import zmq
 import pickle
 from typing import Tuple
@@ -29,7 +26,7 @@ from docopt import docopt
 from pyMilk.interfacing.shm import SHM
 
 
-def zmq_send_loop(host_port: Tuple[str, int], shm_name: str, timeout: float, skip:int = 1):
+def zmq_send_loop(host_port: Tuple[str, int], shm_name: str, timeout: float, skip:int = 1, throttle_sec:float=0.):
 
     # Open shared memories
     shm_obj = SHM(shm_name)
@@ -42,7 +39,14 @@ def zmq_send_loop(host_port: Tuple[str, int], shm_name: str, timeout: float, ski
 
     init = True
     counter = 0
+    t1 = time.time()
+
     while True:
+        t2 = time.time()
+        if t2 - t1 < throttle_sec:
+            time.sleep(throttle_sec - t2 + t1)
+        t1 = time.time()
+
         data = shm_obj.get_data(check=True, checkSemAndFlush=init, timeout=timeout)
         init = False
 
@@ -66,6 +70,11 @@ if __name__ == "__main__":
     port = int(hp[1])
     shm_name = doc['<shm_name>']
     timeout = float(doc['-t'])
+    if doc['-f'] is None:
+        throttle_sec = 0.0
+    else:
+        throttle_sec = 1/float(doc['-f'])
+
     skip = int(doc['-s'])
 
-    zmq_send_loop((host, port), shm_name, timeout, skip=skip)
+    zmq_send_loop((host, port), shm_name, timeout, skip=skip, throttle_sec=throttle_sec)
