4eb05942495fb57c227fbe74bc03d7dce7ae0e3c
vdeo <vdeo@naoj.org>
2024-11-26 17:57:55 -1000

    [tests] AI changes to img_shapes and compliance with tests.

--- pyMilk/util/img_shapes.py
+++ pyMilk/util/img_shapes.py
@@ -42,12 +42,13 @@ This is good for not ovethinking indexing in your code.
 from __future__ import annotations
 
 import numpy as np
+import numpy.typing as npt
 import typing as typ
 
 
-def image_encode(im: np.ndarray, s: int) -> np.ndarray:
+def image_encode(im: npt.NDArray, s: int) -> npt.NDArray:
     # Cases 4 to 7: x and y must be swapped
-    if s > 3:
+    if s > 3 and s <= 7:
         return image_encode(im.T, s - 4)
 
     if s == 0:
@@ -62,11 +63,11 @@ def image_encode(im: np.ndarray, s: int) -> np.ndarray:
         raise ValueError("symcode s must be 0-7")
 
 
-def image_decode(x: np.ndarray, s: int) -> np.ndarray:
+def image_decode(x: npt.NDArray, s: int) -> npt.NDArray:
     return image_encode(x, [0, 1, 2, 3, 4, 6, 5, 7][s])
 
 
-def cube_front_image_encode(cube: np.ndarray, s: int) -> np.ndarray:
+def cube_front_image_encode(cube: npt.NDArray, s: int) -> npt.NDArray:
     """
     Encode image cube - dim 0 is the cube axis
     """
@@ -85,11 +86,11 @@ def cube_front_image_encode(cube: np.ndarray, s: int) -> np.ndarray:
         raise ValueError("symcode s must be 0-7")
 
 
-def cube_front_image_decode(cube: np.ndarray, s: int) -> np.ndarray:
+def cube_front_image_decode(cube: npt.NDArray, s: int) -> npt.NDArray:
     return cube_front_image_encode(cube, [0, 1, 2, 3, 4, 6, 5, 7][s])
 
 
-def cube_back_image_encode(cube: np.ndarray, s: int) -> np.ndarray:
+def cube_back_image_encode(cube: npt.NDArray, s: int) -> npt.NDArray:
     """
     Encode image cube - dim 2 is the cube axis
     """
@@ -108,7 +109,7 @@ def cube_back_image_encode(cube: np.ndarray, s: int) -> np.ndarray:
         raise ValueError("symcode s must be 0-7")
 
 
-def cube_back_image_decode(cube: np.ndarray, s: int) -> np.ndarray:
+def cube_back_image_decode(cube: npt.NDArray, s: int) -> npt.NDArray:
     return cube_back_image_encode(cube, [0, 1, 2, 3, 4, 6, 5, 7][s])
 
 
@@ -117,8 +118,9 @@ Squeezing and unsqueezing with numpy slices
 """
 
 if typ.TYPE_CHECKING:
-    T_ReadSlice = ellipsis | tuple[slice | typ.Literal[0], ...]
-    T_WriteSlice = ellipsis | tuple[slice | None, ...]
+    from types import EllipsisType
+    T_ReadSlice = EllipsisType | tuple[slice | typ.Literal[0], ...]
+    T_WriteSlice = EllipsisType | tuple[slice | None, ...]
     T_RW_Slices = tuple[T_ReadSlice, T_WriteSlice]
 
 
@@ -131,11 +133,9 @@ def gen_squeeze_unsqueeze_slices(shape_orig: tuple[int, ...],
     if not autoSqueeze:
         return np.s_[...], np.s_[...]
 
-    read_slice = tuple((slice(None, None, None), 0)[n == 1]
-                       for n in shape_orig)
-    write_slice = tuple((slice(None, None, None), None)[n == 1]
-                        for n in shape_orig)
-    return read_slice, write_slice
+    read_slice = tuple((slice(None) if n != 1 else 0 for n in shape_orig))
+    write_slice = tuple((slice(None) if n != 1 else None for n in shape_orig))
+    return read_slice, write_slice  # type: ignore -- inference on read_slice is incorrect.
 
 
 """
