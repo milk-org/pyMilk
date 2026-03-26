3d164dbfb3e5fec3f5e9d869987daf8cd6ad22bd
vdeo <vdeo@naoj.org>
2026-03-26 15:02:30 +0100

    update __init__ to print branch abandon warning.

---
+++ pyMilk/__init__.py
@@ -0,0 +1,5 @@
+import warnings
+
+warnings.warn(
+        'pyMilk warning: the `master` branch is abandonned, please switch to `dev` (for ongoing) or `main` (for releases, but these may be pretty rare).'
+)
