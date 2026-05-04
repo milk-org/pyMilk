de42c6d9a2ff777b7f8089af045dc238a427ae9c
vdeo <vdeo@naoj.org>
2026-05-04 13:25:43 +0200

    [tests] GPU non-shared SHM test

--- tests/interfacing/non_shared_shm_test.py
+++ tests/interfacing/non_shared_shm_test.py
@@ -8,14 +8,15 @@ from pyMilk.interfacing.shm import SHM
 
 @pytest.mark.parametrize('shape', [(3, ), (2, 3), (2, 3, 4), (3, 4, 5),
                                    (5, 6, 7)])
-def test_data_conservation(shape: tuple[int, ...]):
+@pytest.mark.parametrize('location', [-1, 0])
+def test_data_conservation(shape: tuple[int, ...], location: int):
 
     for sym in range(8):
         for tri in range(4):
             data: np.ndarray = np.random.randn(*shape)  # type: ignore
 
             shm_write = SHM("pyMilk_autotest", data, symcode=sym, triDim=tri,
-                            location=-1, shared=False)
+                            location=location, shared=False)
 
             shm_write.set_data(data)
             assert np.all(shm_write.get_data() == data)
@@ -27,18 +28,23 @@ def test_data_conservation(shape: tuple[int, ...]):
             shm_write.destroy()
 
 
-def test_shared_nonshared_overlap():
+@pytest.mark.parametrize('location1', [-1, 0])
+@pytest.mark.parametrize('location2', [-1, 0])
+def test_shared_nonshared_overlap(location1: int, location2: int):
     data_shared: np.ndarray = np.random.randn(10, 20).astype(np.float32)
     data_private: np.ndarray = np.random.randn(10, 20).astype(np.float32)
-    shm_shared_1 = SHM('shared_test', data_shared)  # Create
-    shm_private = SHM('shared_test', data_private, shared=False)  # Create
+    shm_shared_1 = SHM('shared_test', data_shared,
+                       location=location1)  # Create
+    shm_private = SHM('shared_test', data_private, location=location2,
+                      shared=False)  # Create
     shm_shared_2 = SHM('shared_test')  # Open
 
     assert np.all(shm_shared_2.get_data() == data_shared)
     assert np.all(shm_private.get_data() == data_private)
     shm_private.destroy()
 
-    shm_private = SHM('shared_test', data_private, shared=False)  # Create
+    shm_private = SHM('shared_test', data_private, location=location2,
+                      shared=False)  # Create
 
     shm_shared_3 = SHM('shared_test')  # Open
     assert np.all(shm_shared_3.get_data() == data_shared)
@@ -48,3 +54,7 @@ def test_shared_nonshared_overlap():
     shm_shared_3.set_data(data_shared - 1)
     assert np.all(shm_shared_1.get_data() == data_shared - 1)
     assert np.all(shm_shared_2.get_data() == data_shared - 1)
+
+    shm_shared_1.close()
+    shm_shared_2.close()
+    shm_shared_3.destroy()
